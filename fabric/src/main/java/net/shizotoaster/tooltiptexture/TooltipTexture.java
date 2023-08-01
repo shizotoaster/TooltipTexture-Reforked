784cc46e5a1afd220225440fe29fc8099baaa98a
Mixer <51480483+shizotoaster@users.noreply.github.com>
2023-08-01 15:30:19 +0700

    Add files via upload

---
+++ fabric/src/main/java/net/shizotoaster/tooltiptexture/TooltipTexture.java
@@ -0,0 +1,11 @@
+package net.shizotoaster.tooltiptexture;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TooltipTexture implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ forge/src/main/java/net/shizotoaster/tooltiptexture/TooltipTexture.java
@@ -0,0 +1,10 @@
+package net.shizotoaster.tooltiptexture;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("tooltiptexture")
+public class TooltipTexture {
+    public TooltipTexture() {
+
+    }
+}
